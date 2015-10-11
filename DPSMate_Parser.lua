c5d480b34dd3fd39d25d2f8cc5c1aa01bca92932
Albea <tomdymel@web.de>
2015-10-11 13:27:03 +0200

    Fixed the parser for existing events, improved performance

--- DPSMate_Parser.lua
+++ DPSMate_Parser.lua
@@ -1,3 +1,6 @@
+-- Notes
+-- "Smbd reflects..." (Thorns etc.)
+
 -- Global Variables
 
 -- Local Variables
@@ -32,10 +35,7 @@ function DPSMate.Parser:OnEvent(event)
 end
 
 function DPSMate.Parser:ParseSelfHits(msg)
-	local target = ""
-	local hit = 0
-	local crit = 0
-	
+	local hit, crit = 0 , 0
 	-- Fall damage
 	if strfind(msg, DPSMate.localization.parser.youfall) then
 		amount = tonumber(strsub(msg, strfind(msg, "%d+")))
@@ -50,210 +50,84 @@ function DPSMate.Parser:ParseSelfHits(msg)
 		DPSMate.DB:BuildUserAbility(player, "Lava", 1, 0, 0, 0, 0, 0, amount, 1)
 	-- White hit Damage
 	elseif strfind(msg, DPSMate.localization.parser.youhit) or strfind(msg, DPSMate.localization.parser.youcrit) then
-		if strfind(msg, DPSMate.localization.parser.youhit) then
-			target = strsub(msg, string.len(DPSMate.localization.parser.youhit)+1, strfind(msg, DPSMate.localization.parser.Dfor)-1)
-			hit = 1
-		else
-			target = strsub(msg, string.len(DPSMate.localization.parser.youcrit)+1, strfind(msg, DPSMate.localization.parser.Dfor)-1)
-			crit = 1
+		for k, t, a in string.gfind(msg, "You (.-) (.+) for (%d+).") do
+			if k == DPSMate.localization.parser.hit then hit=1; else crit=1; end
+			DPSMate.DB:BuildUserAbility(player, "AutoAttack", hit, crit, 0, 0, 0, 0, tonumber(a), 0)
 		end
-		local i, j = strfind(msg, DPSMate.localization.parser.Dfor)
-		local nmsg = strsub(msg, j)
-		local amount = tonumber(strsub(nmsg, strfind(nmsg, "%d+")))
-		DPSMate.DB:BuildUserAbility(player, "AutoAttack", hit, crit, 0, 0, 0, 0, amount, 0)
 	end
 end
 
 function DPSMate.Parser:ParseSelfMisses(msg)
-	local miss = 0
-	local parry = 0
-	local dodge = 0
-	if strfind(msg, DPSMate.localization.parser.youmiss) then
-		miss = 1
-	elseif strfind(msg, DPSMate.localization.parser.parries) then
-		parry = 1
-	elseif strfind(msg, DPSMate.localization.parser.dodges) then
-		dodge = 1
-	end
+	local miss, parry, dodge = 0, 0, 0
+	if strfind(msg, DPSMate.localization.parser.youmiss) then miss = 1; elseif strfind(msg, DPSMate.localization.parser.parries) then parry = 1; elseif strfind(msg, DPSMate.localization.parser.dodges) then dodge = 1; end
 	DPSMate.DB:BuildUserAbility(player, "AutoAttack", 0, 0, miss, parry, dodge, 0, 0, 0)
 end
 
 function DPSMate.Parser:ParseSelfSpellDMG(msg)
-	local target = ""
-	local hit = 0
-	local crit = 0
-	local amount = 0
-	local ability = ""
-	local resist = 0
-	local parry = 0
-	local dodge = 0
-	local miss = 0
-	local i, j
+	local target, hit, crit, amount, ability, resist, parry, dodge, miss, block = "", 0, 0, 0, "", 0, 0, 0, 0, 0
 	if strfind(msg, DPSMate.localization.parser.wasresistedby) then
-		resist = 1
-		i, j = strfind(msg, DPSMate.localization.parser.wasresistedby)
-		ability = strsub(msg, string.len(DPSMate.localization.parser.your)+1, i-1)
-		target = strsub(msg, j+1, string.len(msg)-1)
+		for a, t in string.gfind(msg, "Your (.+) was resisted by (.+).") do resist=1; ability=a; target=t; end
 	elseif strfind(msg, DPSMate.localization.parser.isparriedby) then
-		parry = 1
-		i, j = strfind(msg, DPSMate.localization.parser.isparriedby)
-		ability = strsub(msg, string.len(DPSMate.localization.parser.your)+1, i-1)
-		target = strsub(msg, j+1, string.len(msg)-1)
+		for a, t in string.gfind(msg, "Your (.+) is parried by (.+).") do parry=1; ability=a; target=t; end
 	elseif strfind(msg, DPSMate.localization.parser.wasdodgedby) then
-		dodge = 1
-		i, j = strfind(msg, DPSMate.localization.parser.wasdodgedby)
-		ability = strsub(msg, string.len(DPSMate.localization.parser.your)+1, i-1)
-		target = strsub(msg, j+1, string.len(msg)-1)
+		for a, t in string.gfind(msg, "Your (.+) was dodged by (.+).") do dodge=1; ability=a; target=t; end
 	elseif strfind(msg, DPSMate.localization.parser.missed) then
-		miss = 1
-		i, j = strfind(msg, DPSMate.localization.parser.missed)
-		ability = strsub(msg, string.len(DPSMate.localization.parser.your)+1, i-1)
-		target = strsub(msg, j+1, string.len(msg)-1)
+		for a, t in string.gfind(msg, "Your (.+) missed (.+).") do miss=1; ability=a; target=t; end
+	elseif strfind(msg, DPSMate.localization.parser.wasblockedby) then
+		for a, t in string.gfind(msg, "Your (.+) was blocked by (.+).") do block=1; ability=a; target=t; end
 	elseif strfind(msg, DPSMate.localization.parser.immune) then
 		-- Decided not to collect immune data
 	else
-		if strfind(msg, DPSMate.localization.parser.hits) then
-			i, j = strfind(msg, DPSMate.localization.parser.hits)
-			target = strsub(msg, j+1, strfind(msg, DPSMate.localization.parser.Dfor)-1)
-			ability = strsub(msg, string.len(DPSMate.localization.parser.your)+1, i-1)
-			hit = 1
-		else
-			i, j = strfind(msg, DPSMate.localization.parser.crits)
-			target = strsub(msg, j+1, strfind(msg, DPSMate.localization.parser.Dfor)-1)
-			ability = strsub(msg, string.len(DPSMate.localization.parser.your)+1, i-1)
-			crit = 1
-		end
-		local i, j = strfind(msg, DPSMate.localization.parser.Dfor)
-		local nmsg = strsub(msg, j)
-		amount = tonumber(strsub(nmsg, strfind(nmsg, "%d+")))	
+		-- School and target to be added
+		for ab, t, a in string.gfind(msg, "Your (.+) hits (.+) for (.+).") do ability = ab; target = t; amount = tonumber(strsub(a, strfind(a, "%d+"))); hit=1; end
+		for ab, t, a in string.gfind(msg, "Your (.+) crits (.+) for (.+).") do ability = ab; target = t; amount = tonumber(strsub(a, strfind(a, "%d+"))); crit=1; end
 	end
 	DPSMate.DB:BuildUserAbility(player, ability, hit, crit, miss, parry, dodge, resist, amount, 0)
 end
 
--- Error for (...) suffers from blblblaa's Curse of Agony. (32 resisted)
 function DPSMate.Parser:ParsePeriodicDamage(msg)
-	local target = ""
-	local ability = ""
-	local i, j, i2, j2
 	local cause = {}
-	local amount = 0
-	if strfind(msg, DPSMate.localization.parser.suffers) then
-		target = strsub(msg, 1, strfind(msg, DPSMate.localization.parser.suffers)-1)
-		i, j = strfind(msg, "%b .")
-		ability = strsub(msg, i+1, j-1)
-		i2, j2 = strfind(msg, DPSMate.localization.parser.from)
-		cause.name = strsub(msg, j2+1, i-1)
-		if cause.name == DPSMate.localization.parser.your2 then
-			cause = player
-		else
-			cause.name = strsub(msg, j2+1, i-3)
-		end
-		i, j = strfind(msg, DPSMate.localization.parser.suffers)
-		local nmsg = strsub(msg, j)
-		amount = tonumber(strsub(nmsg, strfind(nmsg, "%d+")))
-		DPSMate.DB:BuildUserAbility(cause, ability.."(Periodic)", 1, 0, 0, 0, 0, 0, amount, 0)
+	-- School has to be added and target
+	for tar, dmg, name, ab in string.gfind(msg, "(.+) suffers (.+) from (.-) (.+).") do
+		cause.name = name
+		if cause.name == DPSMate.localization.parser.your2 then cause = player; else cause.name = strsub(cause.name, 1, strfind(cause.name, "'s")-1); end
+		DPSMate.DB:BuildUserAbility(cause, ab.."(Periodic)", 1, 0, 0, 0, 0, 0, tonumber(strsub(dmg, strfind(dmg, "%d+"))), 0)
 	end
 end
 
 function DPSMate.Parser:ParsePartyHits(msg)
-	local target = ""
-	local cause = {}
-	local amount = 0
-	local i, j
-	local hit = 0
-	local crit = 0
-	
-	if strfind(msg, DPSMate.localization.parser.hits) then
-		cause.name = strsub(msg, 1, strfind(msg, DPSMate.localization.parser.hits)-1)
-		i, j = strfind(msg, DPSMate.localization.parser.hits)
-		hit = 1
-	else
-		cause.name = strsub(msg, 1, strfind(msg, DPSMate.localization.parser.crits)-1)
-		i, j = strfind(msg, DPSMate.localization.parser.crits)
-		crit = 1
+	local cause, hit, crit = {}, 0, 0
+	for c, k, t, a in string.gfind(msg, "(.-) (.-) (.+) for (%d+).") do
+		cause.name = c
+		if k == DPSMate.localization.parser.hits then hit=1; else crit=1; end
+		DPSMate.DB:BuildUserAbility(cause, "AutoAttack", hit, crit, 0, 0, 0, 0, tonumber(a), 0)
 	end
-	target = strsub(msg, j+1, strfind(msg, DPSMate.localization.parser.Dfor)-1)
-	i, j = strfind(msg, "%b .")
-	amount = tonumber(strsub(msg, i+1, j-1))
-	
-	-- Hackfix will be in another event I guess
-	if (not DPSMateUser[cause.name]) then
-		for p=1, 4 do
-			if UnitName("party"..p) == cause.name then
-				cause.class = UnitClass("party"..p)
-				DPSMateUser[cause.name].class = cause.class
-				break
-			end
-		end
-	end
-	
-	DPSMate.DB:BuildUserAbility(cause, "AutoAttack", hit, crit, 0, 0, 0, 0, amount, 0)
 end
 
 function DPSMate.Parser:ParsePartyMisses(msg)
-	local miss = 0
-	local parry = 0
-	local dodge = 0
-	local cause = {}
-	local i, j
-	if strfind(msg, DPSMate.localization.parser.misses) then
-		miss = 1
-	elseif strfind(msg, DPSMate.localization.parser.parries) then
-		parry = 1
-	elseif strfind(msg, DPSMate.localization.parser.dodges) then
-		dodge = 1
-	end
+	local miss, parry, dodge, cause = 0, 0, 0, {}
+	if strfind(msg, DPSMate.localization.parser.misses) then miss = 1; elseif strfind(msg, DPSMate.localization.parser.parries) then parry = 1; elseif strfind(msg, DPSMate.localization.parser.dodges) then dodge = 1; end
 	cause.name = strsub(msg, 1, strfind(msg, "%s")-1)
 	DPSMate.DB:BuildUserAbility(cause, "AutoAttack", 0, 0, miss, parry, dodge, 0, 0, 0)
 end
 
 function DPSMate.Parser:ParsePartySpellDMG(msg)
-	local target = ""
-	local ability = ""
-	local cause = {}
-	local i, j, i2, j2
-	local hit = 0
-	local crit = 0
-	local amount = 0
-	-- cases are missing
-	local resist = 0
-	local parry = 0
-	local dodge = 0
-	local miss = 0
-	
-	i2, j2 = strfind(msg, "%s")
+	local target, ability, cause, hit, crit, amount, resist, parry, dodge, miss, block = "", "", {}, 0, 0, 0, 0, 0, 0, 0, 0
 	if strfind(msg, DPSMate.localization.parser.wasdodgedby) then
-		ability = strsub(msg, i2+1, strfind(msg, DPSMate.localization.parser.wasdodgedby)-1)
-		target = strsub(msg, strfind(msg, "%b ."))
-		dodge=1
+		for c, ab, t in string.gfind(msg, "(.-)'s (.+) was dodged by (.+).") do dodge=1; cause.name=c; ability=ab; target=t; end
 	elseif strfind(msg, DPSMate.localization.parser.wasparriedby) then
-		ability = strsub(msg, i2+1, strfind(msg, DPSMate.localization.parser.wasparriedby)-1)
-		target = strsub(msg, strfind(msg, "%b ."))
-		parry=1
+		for c, ab, t in string.gfind(msg, "(.-)'s (.+) was parried by (.+).") do parry=1; cause.name=c; ability=ab; target=t; end
 	elseif strfind(msg, DPSMate.localization.parser.wasresistedby) then
-		ability = strsub(msg, i2+1, strfind(msg, DPSMate.localization.parser.wasresistedby)-1)
-		target = strsub(msg, strfind(msg, "%b ."))
-		resist=1
+		for c, ab, t in string.gfind(msg, "(.-)'s (.+) was resisted by (.+).") do resist=1; cause.name=c; ability=ab; target=t; end
 	elseif strfind(msg, DPSMate.localization.parser.missed) then
-		ability = strsub(msg, i2+1, strfind(msg, DPSMate.localization.parser.missed)-1)
-		target = strsub(msg, strfind(msg, "%b ."))
-		miss=1
+		for c, ab, t in string.gfind(msg, "(.-)'s (.+) missed (.+).") do miss=1; cause.name=c; ability=ab; target=t; end
+	elseif strfind(msg, DPSMate.localization.parser.wasblockedby) then
+		for c, ab, t in string.gfind(msg, "(.-)'s (.+) was blocked by (.+).") do block=1; cause.name=c; ability=ab; target=t; end
+	elseif strfind(msg, DPSMate.localization.parser.immune) then
+		-- Decided not to collect immune data
 	else
-		if strfind(msg, DPSMate.localization.parser.hits) then
-			i, j = strfind(msg, DPSMate.localization.parser.hits)
-			ability = strsub(msg, i2+1, i-1)
-			hit = 1
-		else
-			i, j = strfind(msg, DPSMate.localization.parser.crits)
-			ability = strsub(msg, i2+1, i-1)
-			crit = 1
-		end
-		target = strsub(msg, j+1, strfind(msg, DPSMate.localization.parser.Dfor)-1)
-		i, j = strfind(msg, DPSMate.localization.parser.Dfor)
-		local nmsg = strsub(msg, j)
-		amount = tonumber(strsub(nmsg, strfind(nmsg, "%d+")))
+		for c, ab, t, a in string.gfind(msg, "(.-)'s (.+) hits (.+) for (.+).") do hit=1; cause.name=c; ability=ab; target=t; amount=tonumber(strsub(a, strfind(a, "%d+"))); end
+		for c, ab, t, a in string.gfind(msg, "(.-)'s (.+) crits (.+) for (.+).") do crit=1; cause.name=c; ability=ab; target=t; amount=tonumber(strsub(a, strfind(a, "%d+"))); end
 	end
-	cause.name = strsub(msg, 1, i2-3)
-	
 	DPSMate.DB:BuildUserAbility(cause, ability, hit, crit, miss, parry, dodge, resist, amount, 0)
 end
