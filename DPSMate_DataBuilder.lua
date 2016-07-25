f2835c06e5e944fac8bf49e0acef67b28a030579
Albea <tomdymel@web.de>
2016-07-25 23:47:46 +0200

    Fixed nil issue

--- DPSMate_DataBuilder.lua
+++ DPSMate_DataBuilder.lua
@@ -648,6 +648,7 @@ function DPSMate.DB:OnGroupUpdate()
 	local pet = UnitName("pet")
 	local name = UnitName("player")
 	if pet and pet ~= DPSMate.L["unknown"] then
+		self:BuildUser(name, nil)
 		DPSMateUser[pet][4] = true
 		DPSMateUser[name][5] = pet
 		DPSMateUser[pet][6] = DPSMateUser[name][1]
