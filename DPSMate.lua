8d7d51bdd067aafefb5979a0c23e1c7541240f29
Albea <tomdymel@web.de>
2016-08-19 15:50:26 +0200

    ?

--- DPSMate.lua
+++ DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 52
+DPSMate.VERSION = 53
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
