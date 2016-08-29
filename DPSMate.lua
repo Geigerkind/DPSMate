57fa5b9103b62de72fd607bf7f054c897d62cc42
Albea <tomdymel@web.de>
2016-08-29 18:24:12 +0200

    Build 61. From here on just some bugfixes

--- DPSMate.lua
+++ DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 60
+DPSMate.VERSION = 61
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
