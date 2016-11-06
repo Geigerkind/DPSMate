3f6aae00d27539e77ca9eb7d87d9fe0b190debc8
Albea <tomdymel@web.de>
2016-11-06 13:39:39 +0100

    little fix

--- DPSMate_Options.lua
+++ DPSMate_Options.lua
@@ -1603,18 +1603,19 @@ end
 function DPSMate.Options:NewSegment(segname)
 	-- Get name of this session
 	local _,_,a = DPSMate.Modules.EDT:GetSortedTable(DPSMateEDT[2])
+	local extra = ""
 	if a[1] or segname~=nil then
 		local name = segname
 		if not segname then
 			name = DPSMate:GetUserById(a[1]) or DPSMate.L["unknown"]
-			name = name.." - CBT: "..self:FormatTime(DPSMateCombatTime["current"])
+			extra = " - CBT: "..self:FormatTime(DPSMateCombatTime["current"])
 		end
 		if DPSMateSettings["onlybossfights"] then
 			if DPSMate.BabbleBoss:Contains(name) then
-				DPSMate.Options:CreateSegment(name)
+				DPSMate.Options:CreateSegment(name..extra)
 			end
 		else
-			DPSMate.Options:CreateSegment(name)
+			DPSMate.Options:CreateSegment(name..extra)
 		end
 		
 		DPSMateDamageDone[2] = {}
