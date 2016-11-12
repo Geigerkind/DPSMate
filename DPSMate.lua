f27c20170ab61a52a118731b11e3ef4e197ecb5c
Albea <tomdymel@web.de>
2016-11-12 13:55:07 +0100

    Build 81: German parser fixes

--- DPSMate.lua
+++ DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 80
+DPSMate.VERSION = 81
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
