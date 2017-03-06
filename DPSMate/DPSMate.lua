ef8acdf069cc81aba6ba43c85619094095048927
Albea <tomdymel@web.de>
2017-03-06 09:46:23 +0100

    Hotfix stupid lua error!

--- DPSMate/DPSMate.lua
+++ DPSMate/DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 93
+DPSMate.VERSION = 94
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = {}
