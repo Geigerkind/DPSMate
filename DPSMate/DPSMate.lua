c7649f3c1b7f18575ddd5c2f97fdca3f5d32bb1c
Albea <tomdymel@web.de>
2017-04-10 19:08:23 +0200

    Build update

--- DPSMate/DPSMate.lua
+++ DPSMate/DPSMate.lua
@@ -1,6 +1,6 @@
 -- Global Variables
 DPSMate = {}
-DPSMate.VERSION = 109
+DPSMate.VERSION = 110
 DPSMate.LOCALE = GetLocale()
 DPSMate.SYNCVERSION = DPSMate.VERSION..DPSMate.LOCALE
 DPSMate.Parser = CreateFrame("Frame", nil, UIParent)
